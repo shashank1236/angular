c5e5150730290258bed44d7d3f4c8b59e62a30c8
Shashankmittal123 <my.shashank12@gmail.com>
2022-08-13 23:15:03 +0530

    13-08-2022/ 23:00

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+//   template: `<app-employee>
+// </app-employee><app-employee>
+// </app-employee>`,
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
